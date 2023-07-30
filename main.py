8ecd9ef52560fe4127c3b6cb248bbe25615605cd
ZeinHalawi <zein.aa.halawi@gmail.com>
2023-07-30 13:51:33 +0300

    book ticket (done)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #importing a text file (https://www.youtube.com/watch?v=0EgSo7hsRWM):
 file = open("zein text.txt", "r")
 
-f = file.readlines(
-)  # change file into list to better extract from it (every line)
+f = file.readlines()  # change file into list to better extract from it (every line)
 
 
 list = []
@@ -41,6 +40,9 @@ for line in f:
 #defining 2 functions for user and admin menus
 def displayAdminMenu():
   print("kindly choose from 1 to 7 \n1. Display Statistics\n2. Book a Ticket\n3. Display all Tickets\n4. Change Ticket's Priority\n5. Disable Ticket\n6. Run Events\n7. Exit\n")
+  
+
+  
 def displayUserMenu():
   print("kindly choose between the 2 options\n1. Book a ticket\n2. Exit\n")  
 
@@ -71,6 +73,21 @@ def displayStatistics():
 
   print("this is the event with the highest count of tickets",maxevent)
 
+def bookTicket():
+    new_tick=int(list[-1]['tick'][4:])+1 
+    new_ticket_String = "tick" + str(new_tick)
+    new_event=input("please enter an event number:")
+    new_username=input("please enter a name:")
+    new_date=input("please enter date:")
+    new_priority=input("please enter priority number:")
+    new_ticket={}
+    values=[new_ticket_String,new_event,new_username,new_date,new_priority]
+    for i in range(len(values)):
+      new_ticket[keys[i]]=values[i]
+    list.append(new_ticket)  
+      
+    print("New list contating the new ticket is:\n",list)  
+
 def main():
   username = input("Hello please enter a username:")
   password = input("Please enter your password:")
@@ -79,6 +96,20 @@ def main():
     counter = 0
     if password == "admin123123":
       print("welcome admin")
+      displayAdminMenu()
+      choice=int(input()) 
+      while choice != 7:
+        if choice == 1:
+          displayStatistics()
+        elif choice == 2:
+          bookTicket()  
+        else:
+          print("!!!invaild input") 
+        displayAdminMenu()
+        choice=int(input())   
+      print("You have exited the program")  
+
+      
       break
     else:
       while password != "admin123123":# to not allow the admin to try more than 5 times with wrong password
@@ -99,4 +130,5 @@ def main():
   
   if username != "admin":
     print("Hello", username)
+    displayUserMenu()
 main()  
